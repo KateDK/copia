0d1148d1a03497211441318eafc6d07fc3d4f73d
Kate Dubitski <kate.dubitski@gmail.com>
2020-12-21 11:12:26 -0500

    Import sync and use to short-circuit and return false if unique vals exist

--- equality.js
+++ equality.js
@@ -6,19 +6,42 @@
  *
  */
 
-const equals = () => {};
+let sync = require('./sync');
 
-const testEquals = () => {
 const equals = (obj1, obj2) => {
   //Want to get array of keys to loop over
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
+  //if length of both keys arrays in not equal than return false
+  if (keys1.length !== keys2.length) return false;
+  //if any of the keys are not equal, return false
+  if (sync(keys1, keys2).length) return false;
+};
+
+const testEquals = (obj1, obj2) => {
   console.log(
     equals(
       { key1: 'value', key2: { key3: 1 } },
       { key1: 'value', key2: { key3: 1 } }
     ),
     ' ==> true'
+  );
+
+  console.log(
+    equals(
+      { key1: 'value1', key2: { key3: 1 }, key4: 'value2' },
+      { key1: 'value', key2: { key3: 1 } }
+    ),
+    ' ==> false'
+  );
+
+  //test for same number of keys but different key names
+  console.log(
+    equals(
+      { key1: 'value1', key2: { key3: 1 }, key4: 'value2' },
+      { key1: 'value', key2: { key3: 1 }, key5: 'value2' }
+    ),
+    ' ==> false'
   );
 };
 
